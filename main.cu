b97b911f6d66d00e3846429487fe833a6048e146
Kornel Skorka <kornel1902@gmail.com>
2020-12-19 23:47:41 +0100

    refactor

--- main.cu
+++ main.cu
@@ -21,36 +21,6 @@ void generateSequences(int *sequences, const int count) {
     }
 }
 
-int count_setbits(int N) {
-    int cnt = 0;
-    while (N > 0) {
-        cnt += N & 1;
-        N = N >> 1;
-    }
-    return cnt;
-}
-
-bool hammingDistanceOne(const int *sequences, int i, int j) {
-    int hamming = 0;
-    // sum hamming distance for each sequence part
-    for (int offset = 0; offset < SEQUENCE_LEN; offset++) {
-        // increment by hamming distance of the parts
-        hamming += count_setbits(sequences[i * SEQUENCE_LEN + offset] ^ sequences[j * SEQUENCE_LEN + offset]);
-        //if greater than one fail fast
-        if (hamming > 1) return false;
-    }
-    return hamming == 1;
-}
-
-void hammingWithCPU(const int *sequences, bool *pairs, int count) {
-    for (int i = 0; i < count; i++) {
-        for (int j = i + 1; j < count; j++) {
-            if (hammingDistanceOne(sequences, i, j))
-                pairs[i * count + j] = true;
-        }
-    }
-}
-
 void printPairs(const bool *isPair, const int *seqs) {
     int counter = 0;
     for (int i = 0; i < SEQUENCE_COUNT * SEQUENCE_COUNT; i++) {
@@ -93,6 +63,38 @@ struct timespec& end)
                           ? (end.tv_nsec - start.tv_nsec) / 1e6 + (end.tv_sec - start.tv_sec) * 1e3
                           : (start.tv_nsec - end.tv_nsec) / 1e6 + (end.tv_sec - start.tv_sec - 1) * 1e3;
     printf("%s: %lf milliseconds\n", prefix, milliseconds);
+}
+
+///////////////////////////// CPU
+
+int count_setbits(int N) {
+    int cnt = 0;
+    while (N > 0) {
+        cnt += N & 1;
+        N = N >> 1;
+    }
+    return cnt;
+}
+
+bool hammingDistanceOne(const int *sequences, int i, int j) {
+    int hamming = 0;
+    // sum hamming distance for each sequence part
+    for (int offset = 0; offset < SEQUENCE_LEN; offset++) {
+        // increment by hamming distance of the parts
+        hamming += count_setbits(sequences[i * SEQUENCE_LEN + offset] ^ sequences[j * SEQUENCE_LEN + offset]);
+        //if greater than one fail fast
+        if (hamming > 1) return false;
+    }
+    return hamming == 1;
+}
+
+void hammingWithCPU(const int *sequences, bool *pairs, int count) {
+    for (int i = 0; i < count; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (hammingDistanceOne(sequences, i, j))
+                pairs[i * count + j] = true;
+        }
+    }
 }
 
 //////////////////////////// GPU
